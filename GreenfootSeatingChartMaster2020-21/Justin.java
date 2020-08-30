534fcdb15d74db7953cc9799f30b74cd5e249049
JustinFok1 <justinfok1@gmail.com>
2020-08-29 20:20:41 -0700

    add exisiting work

---
+++ GreenfootSeatingChartMaster2020-21/Justin.java
@@ -0,0 +1,26 @@
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Justin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Justin extends Student
+{
+    /**
+     * Act - do whatever the Justin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Justin()
+    {
+        getImage().scale(getImage().getWidth()/10, getImage().getHeight()/10);
+        getImage().rotate(90);
+    }
+
+    public void act() 
+    {
+        
+    }    
+}
---
+++ GreenfootSeatingChartMaster2020-21/MrKaehms.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MrKaehms here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MrKaehms extends Teacher
+{
+    /**
+     * Act - do whatever the MrKaehms wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
